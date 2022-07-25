f4c2c4d3832e4d4856b9c68fa8b64973a6415208
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-25 10:11:00 -0400

    Add cicd.groovy script

---
+++ cicd.groovy
@@ -0,0 +1,15 @@
+node('linux') 
+{
+        stage ('Poll') {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/lz4port.git']]])
+        }
+
+        stage('Build') {
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/lz4port.git'), string(name: 'PORT_DESCRIPTION', value: 'LZ4 is a lossless data compression algorithm that is focused on compression and decompression speed.' )]
+        }
+}
