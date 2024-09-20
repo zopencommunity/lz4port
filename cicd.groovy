0cbbc590256d7e840e4944002475970169280958
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:43 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/lz4port.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/lz4port.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/lz4port.git'), string(name: 'PORT_DESCRIPTION', value: 'LZ4 is a lossless data compression algorithm that is focused on compression and decompression speed.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/lz4port.git'), string(name: 'PORT_DESCRIPTION', value: 'LZ4 is a lossless data compression algorithm that is focused on compression and decompression speed.' )]
         }
 }
